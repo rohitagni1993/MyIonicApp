9ec7d9620c2257c5dbee0064473f38ca50586526
Rohitagni Mukherjee <rohitagni1993@live.com>
2018-08-29 23:25:56 +0530

    Initial copy
    Initial copy

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { Page1Page } from '../pages/page1/page1';
+import { Page2Page } from '../pages/page2/page2';
+import { Page3Page } from '../pages/page3/page3';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes : Routes =  [
+  { path: "page1", component: Page1Page },
+  { path: "page2", component: Page2Page },
+  { path: "page3", component: Page3Page }
+];
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    Page1Page,
+    Page2Page,
+    Page3Page
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    RouterModule.forRoot(appRoutes)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    Page1Page,
+    Page2Page,
+    Page3Page
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
